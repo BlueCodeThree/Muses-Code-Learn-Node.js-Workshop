c942335736292975ffafcb0acba9322d8280dc08
BlueCodeThree <47844116+BlueCodeThree@users.noreply.github.com>
2019-03-02 11:26:10 +1000

    created templates, server, routes....

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ var server = express();
 var staticAssets = express.static('public');
 server.use(staticAssets);
 
+server.set('view engine', 'ejs');
+
+server.get('/hello', function(request, response) {
+    response.send('hello there');
+});
+
+server.get('/morning', function(request, response) {
+    response.send('Good Morning! How are you?');
+});
+
+server.get('/', function(request, response) {
+    response.render('pages/index');
+});
+    
+    
+server.get('/about', function(request, response) {
+    response.render('pages/about');
+});
+
 server.listen(3000, function () {
     console.log('Server has started listening on port 3000.');
-  });
+});
+
+
+
